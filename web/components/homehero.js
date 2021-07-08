b1465bc0f358c1ce710cd1f67d6bff802a40aee5
J.C <jimbomoso@gmail.com>
2021-07-08 17:06:29 -0400

    Adds !

--- web/components/homehero.js
+++ web/components/homehero.js
@@ -5,7 +5,7 @@ export default function Homehero() {
   return (
       <div className="relative">
           <div className="absolute inset-0 z-10 flex justify-center items-end p-10 text-6xl text-white font-semibold">
-            <p> Welcome to Northeast Rod Run</p>
+            <p> Welcome to Northeast Rod Run!</p>
           </div>
         <Image className="md:w-full lg:w-full" src={rodrun4} />
       </div>
