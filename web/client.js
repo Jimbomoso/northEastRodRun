063fb58360af337871798161b7513a1d8c2a075c
J.C <jimbomoso@gmail.com>
2021-07-19 14:34:21 -0400

    Updates comments engine

--- web/client.js
+++ web/client.js
@@ -1,8 +1,8 @@
-// client.js
-import sanityClient from '@sanity/client'
+// // client.js
+// import sanityClient from '@sanity/client'
 
-export default sanityClient({
-  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID, // you can find this in sanity.json
-  dataset: process.env.SANITY_STUDIO_API_DATASET || "production", // or the name you chose in step 1
-  useCdn: false // `false` if you want to ensure fresh data
-})
+// export default sanityClient({
+//   projectId: process.env.SANITY_STUDIO_API_PROJECT_ID, // you can find this in sanity.json
+//   dataset: process.env.SANITY_STUDIO_API_DATASET || "production", // or the name you chose in step 1
+//   useCdn: false // `false` if you want to ensure fresh data
+// })
--- web/lib/sanity.js
+++ web/lib/sanity.js
@@ -4,10 +4,10 @@ import sanityImage from '@sanity/image-url'
 const options = {
   // Find your project ID and dataset in `sanity.json` in your studio project
   // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID="z6z6hq4k",
-  dataset: process.env.SANITY_PUBLIC_SANITY_DATASET || "production",
-  useCdn: process.env.NODE_ENV === "production",
-  apiVersion: '2021-03-25',
+  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID="z6z6hq4k",
+  dataset: process.env.SANITY_STUDIO_API_DATASET="production",
+  useCdn: false,
+  apiVersion: '2021-07-14',
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
